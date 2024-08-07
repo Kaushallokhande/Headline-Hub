9127c8e7eff89649ee655ab635a0a4dfb6ada418
Kaushal <kaushallokhande12345@gamil.com>
2024-08-07 13:13:56 +0530

    update

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import BackToTopButton from './components/BackToTopButton';
 import useLocalStorage from 'use-local-storage';
 
 const App = () => {
-  let pageSize = 9;
-  let apikey = "1149dea25d5945388fbbd876f7cbdbd1";
-  let country = 'in';
+  const pageSize = 9;
+  const apikey = "1149dea25d5945388fbbd876f7cbdbd1";
+  const country = 'in';
   const [progress, setProgress] = useState(0);
   const [isDark, setDark] = useLocalStorage("isDark", false);
   const [articles, setArticles] = useState([]);
@@ -19,19 +19,16 @@ const App = () => {
     <BrowserRouter>
       <div className='App' data-theme={isDark ? 'dark' : 'light'}>
         <NavBar isDark={isDark} articles={articles} setArticles={setArticles} setDark={setDark} />
-        <LoadingBar
-          color='#f11946'
-          progress={progress}
-        />
+        <LoadingBar color='#f11946' progress={progress} />
         <Routes>
-          <Route path="/" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="general" head="" isDark={isDark} articles={articles} setArticles={setArticles} setDark={setDark} />} />
-          <Route path="/business" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="business" head=": Business" articles={articles} setArticles={setArticles} />} />
-          <Route path="/entertainment" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="entertainment" head=": Entertainment" articles={articles} setArticles={setArticles} />} />
-          <Route path="/general" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="general" head=": General" articles={articles} setArticles={setArticles} />} />
-          <Route path="/health" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="health" head=": Health" articles={articles} setArticles={setArticles} />} />
-          <Route path="/science" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="science" head=": Science" articles={articles} setArticles={setArticles} />} />
-          <Route path="/sports" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="sports" head=": Sports" articles={articles} setArticles={setArticles} />} />
-          <Route path="/technology" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="technology" head=": Technology" articles={articles} setArticles={setArticles} />} />
+          <Route path="/" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="general" isDark={isDark} articles={articles} setArticles={setArticles} />} />
+          <Route path="/business" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="business" isDark={isDark} articles={articles} setArticles={setArticles} />} />
+          <Route path="/entertainment" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="entertainment" isDark={isDark} articles={articles} setArticles={setArticles} />} />
+          <Route path="/general" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="general" isDark={isDark} articles={articles} setArticles={setArticles} />} />
+          <Route path="/health" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="health" isDark={isDark} articles={articles} setArticles={setArticles} />} />
+          <Route path="/science" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="science" isDark={isDark} articles={articles} setArticles={setArticles} />} />
+          <Route path="/sports" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="sports" isDark={isDark} articles={articles} setArticles={setArticles} />} />
+          <Route path="/technology" element={<News setProgress={setProgress} apikey={apikey} pageSize={pageSize} country={country} category="technology" isDark={isDark} articles={articles} setArticles={setArticles} />} />
         </Routes>
         <BackToTopButton />
       </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,22 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import InfiniteScroll from "react-infinite-scroll-component";
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
-
 
 const News = (props) => {
     const { isDark, articles, setArticles } = props;
     const [loading, setLoading] = useState(true);
     const [page, setPage] = useState(1);
-    const [totalResults, setTotalResults] = useState(0);
 
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
-
-    const updateNew = async () => {
+    const updateNews = async () => {
         try {
             props.setProgress(10);
             const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page}&pageSize=${props.pageSize}`;
@@ -25,9 +21,8 @@ const News = (props) => {
             let response = await fetch(url);
             props.setProgress(35);
             let data = await response.json();
-            setArticles(data.articles)
-            setTotalResults(data.totalResults)
-            setLoading(false)
+            setArticles(data.articles);
+            setLoading(false);
             props.setProgress(100);
         } catch (error) {
             console.error('Error fetching data:', error);
@@ -36,59 +31,31 @@ const News = (props) => {
 
     useEffect(() => {
         document.title = `${capitalizeFirstLetter(props.category)} - NewsPaper`;
-        updateNew();
+        updateNews();
         //eslint-disable-next-line
     }, [])
 
-
-
-    const fetchMoreData = async () => {
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page + 1}&pageSize=${props.pageSize}`;
-        setPage(page + 1);
-        let response = await fetch(url);
-        let data = await response.json();
-        setArticles(articles.concat(data.articles))
-        setTotalResults(data.totalResults)
-    };
-
     return (
-        <div className='container my-3' >
-            <h2 className='text-center' id='heading'>Top headlines{props.head}</h2>
+        <div className='container my-3'>
+            <h2 className='text-center'>Top headlines {props.head}</h2>
             {loading && <Spinner />}
-            <InfiniteScroll
-                dataLength={articles.length}
-                next={fetchMoreData}
-                hasMore={articles.length !== totalResults}
-                loader={<Spinner />}
-            >
-                <div className='container' >
-                    <div className='row'>
-                        {articles.map((e) => {
-
-                            return (
-                                <div className='col-md-4'>
-                                    <NewsItem
-                                        key={e.url}
-                                        title={e.title ? e.title.slice(0, 44) : ""}
-                                        description={e.description ? e.description.slice(0, 88) : ""}
-                                        imageurl={e.urlToImage ? e.urlToImage : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMoAAACUCAMAAADlP0YdAAAAXVBMVEXDw8MAAADKysqGhobHx8dycnJtbW2lpaXOzs4oKCglJSViYmK9vb1qampPT0+wsLCQkJCWlpadnZ0bGxsKCgo8PDxGRkaAgIBcXFwSEhJUVFQ2NjZ5eXkwMDAXFxcUuSY4AAAB60lEQVR4nO3Y226jMBRAUeIcCqGAXXJp0qb9/88ch8skXEY1fZgcS3s9VZEqeQtsbCcJAAAAAAAAAAAAAAAAAAAAAHVMvpYxzx7zsjKtXtZKy2ePeonZbn5hq/G5+JR6u1KtNiWVlVK9Ke0ffoih/6M7RZKssaHjU51i7MeuOLrAAWpOEXu8rUuHwBbVKe/dGvsR9r3QnJJUXcqnDZr6qlOuXcppnmLMvE5zimT7NuU6H/bbSzb7UXNKIq7wJWk5HbX4bc1+1qI6xa9hbtsks5L2aX1OFwPdKbeP/exFEuneuzof/648ZYm8Lm+E40sxbtjTT6ZLdClid3/PJ6fRghBHyn3GSHl8OGudo0sx1g1fybwanRsft2cxpBi/rew3L/nb+Ai8fzgCRJAiZe0HvbuliP2aHOfr+3SJIKXfVVZ+zG3UWBVRSt4vvgcnJl24ZnHDl1J9ipSHfsxfNlso2RyGV0x7iiT3xfe0VLLZHPtNmvqUpXdq4hpFijSXn1O+m7ZFd4qUASXDdFGe8o/pMXVSnzJcufzsXXSnmOw7NOW231ecIrYILfH7Gqv5+tu482uwszOKU4Kv8DuKn8qlWOmiM0WaYrda0ax8kP+HZL+gsqS7AVvr2WMGAAAAAAAAAAAAAAAAAADAs/0B6LcaZlErLnYAAAAASUVORK5CYII='}
-                                        readUrl={e.url}
-                                        author={e.author ? e.author : 'Unknown'}
-                                        publishedAt={e.publishedAt ? e.publishedAt : 'not avaliable'} isDark={isDark}
-                                    />
-                                    <br />
-                                </div>
-                            );
-                        })}
+            <div className='row'>
+                {articles.map((e, index) => (
+                    <div className='col-md-4' key={index}>
+                        <NewsItem
+                            title={e.title ? e.title.slice(0, 44) : ""}
+                            description={e.description ? e.description.slice(0, 88) : ""}
+                            imageurl={e.urlToImage ? e.urlToImage : 'default_image_url'}
+                            readUrl={e.url}
+                            author={e.author ? e.author : 'Unknown'}
+                            publishedAt={e.publishedAt ? e.publishedAt : 'not available'}
+                            isDark={isDark}
+                        />
                     </div>
-                </div>
-            </InfiniteScroll>
-
-
-            {/*if needed btn area */}
+                ))}
+            </div>
         </div>
-    )
-
+    );
 }
 
 News.defaultProps = {
@@ -102,8 +69,12 @@ News.propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
-    head: PropTypes.string
+    head: PropTypes.string,
+    setProgress: PropTypes.func.isRequired,
+    apikey: PropTypes.string.isRequired,
+    isDark: PropTypes.bool.isRequired,
+    articles: PropTypes.array.isRequired,
+    setArticles: PropTypes.func.isRequired
 };
 
-export default News
-// isDark === 'dark' ? '#f8f9fa' : '#343a40'
+export default News;
